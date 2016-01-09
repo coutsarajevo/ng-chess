83d53a02d20c9ca3fde7ec13be394469f5e2745a
Harun Smrkovic <harun@personal.com>
2016-01-09 13:50:18 +0100

    initial yo scaffold

---
+++ app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name chessApp
+ * @description
+ * # chessApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('chessApp', [
+    'ngCookies',
+    'ngResource',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/board.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name chessApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the chessApp
+ */
+angular.module('chessApp')
+  .controller('BoardCtrl', function () {
+
+  });
